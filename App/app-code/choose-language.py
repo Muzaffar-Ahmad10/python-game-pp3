30ec4a82871d306eeaea42f938837014af85a8cb
Muzaffar Ahmad Tahir <muzaffarahmad709@gmail.com>
2024-03-19 14:53:03 +0000

    Add view folder and selection topics options

---
+++ App/app-code/choose-language.py
@@ -0,0 +1,23 @@
+
+"""
+ this function is actually taking the arguements from the user to show him
+ questions about the selected lanuage or choosen option.
+"""
+def choose_language():
+    print("\n\n\033[95mChoose a language for the quiz:\033[0m\n\n")
+    print("\033[94m1. HTML\n2. CSS\n3. Python\n4. JavaScript\n5. C++\033[0m")
+    language_choice = input("\n\n\033[1mEnter the number corresponding to your choice: \033[0m")
+
+    #User can select one of the following options:
+    languages = {
+        '1': 'HTML',
+        '2': 'CSS',
+        '3': 'Python',
+        '4': 'JavaScript',
+        '5': 'C++'
+    }
+
+    language = languages.get(language_choice)
+    if not language:
+        print("\033[91mInvalid choice. Please try again.\033[0m")
+    return language
--- run.py
+++ run.py
@@ -1,3 +1,40 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import sys
+import os
+
+views_folder = os.path.join(os.getcwd(), 'views')  # Get current working directory
+sys.path.append(views_folder)
+
+from user_auth.auth import main as user_auth_main
+
+def main():
+    print("\n\033[95mWelcome to CoderQuiz!\033[0m") 
+
+     """ 
+     Header in magenta color
+     print("\033[94m1. Sign Up\n2. Log In\n3. Change Password\n4. Exit\033[0m")  
+     Menu options in blue color
+
+     """
+    print("Type Y or Yes to start the Quiz App.\n\n")
+    choice = input("\033[1mEnter Yes or No: \033[0m").lower()  
+    print(choice)
+
+    """
+     Bold prompt for user input
+    """
+
+    if choice == "y" or choice == "yes":
+        """
+         Call user authentication main function
+        """
+        user_auth_main()
+        
+    elif choice == 'n' or choice == "no":
+        print("\033[92mExiting...\033[0m")  # Green color for "Exiting..."
+        sys.exit()
+    else:
+        print("\n\n\033[91mInvalid choice. Please try again.\033[0m\n\n")  # Red color for error message
+if __name__ == "__main__":
+    main()
+
+    
